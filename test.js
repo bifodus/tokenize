ab3b5dbf5919cc8e116a57167d7187ccf5bf2828
bifodus <bifodus@gmail.com>
2014-07-13 00:47:06 -0700

    setting up mocha

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
-tokenize('dad cat', [{type: "variable", regex: /[a-z]+/},{type:"whitespace", regex: /\s/}], cb){}
+//tokenize('dad cat', [{type: "variable", regex: /[a-z]+/},{type:"whitespace", regex: /\s/}], cb){}
 
-tokenize('dad 5 cat 234847', [{type: "variable", regex: /[a-z]+/},{type:"whitespace", regex: /\s/}]);
+//tokenize('dad 5 cat 234847', [{type: "variable", regex: /[a-z]+/},{type:"whitespace", regex: /\s/}]);
+describe('tokenize', function() {
+  var tokenize = require('./');
+  it('should be a function', function() {
+    tokenize.should.be.type('function');
+  })
+})
+
