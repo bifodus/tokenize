2c84c964197b923a67fdeaa5e5bcfaa1c59bf8a6
bifodus <bifodus@gmail.com>
2014-07-14 00:41:06 -0700

    adding test cases

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ describe('tokenize', function() {
   var tokenize = require('./');
   it('should be a function', function() {
     tokenize.should.be.type('function');
-  })
+  });
   it('should return an array of results', function(done) {
     tokenize('dad cat', [{type: "variable", regex: /[a-z]+/},{type:"whitespace", regex: /\s/}], function(err, results){
       results.should.be.an.instanceOf(Array);
@@ -73,5 +73,78 @@ describe('tokenize', function() {
       done(err);
     });
   });
-})
+  it('should handle repetitive tokens', function(done){
+    var tokenSpecs = [
+      {type: 'a', regex: /a/},
+      {type: 'b', regex: /b/}
+    ];
+    tokenize('abab', tokenSpecs, function(err, results){
+      assert.deepEqual(results, [
+        {
+          type: 'a',
+          value: 'a',
+          index: 0
+        },
+        {
+          type: 'b',
+          value: 'b',
+          index: 1
+        },
+        {
+          type: 'a',
+          value: 'a',
+          index: 2
+        },
+        {
+          type: 'b',
+          value: 'b',
+          index: 3
+        }
+      ]);
+      done(err);
+    });
+  });
 
+  it('should return an array of bad tokens', function(done) {
+    tokenize('dad 5 cat 234847', [{type: "variable", regex: /[a-z]+/},{type:"whitespace", regex: /\s/}], function(err, results){
+      assert.deepEqual(results, [
+        {
+          type: "variable",
+          value: "dad",
+          index: 0
+        },
+        {
+          type: "whitespace",
+          value: " ",
+          index: 3
+        },
+        {
+          type: "whitespace",
+          value: " ",
+          index: 5
+        },
+        {
+          type: "variable",
+          value: "cat",
+          index: 6
+        },
+        {
+          type: "whitespace",
+          value: " ",
+          index: 9
+        }
+      ]);
+      assert.deepEqual(err, [
+        {
+          value: "5",
+          index: 4
+        },
+        {
+          value: "234847",
+          index: 10
+        },
+      ]);
+      done();
+    });
+  });
+});
